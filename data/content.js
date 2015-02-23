b04bfb73dd70b21463418286b76db2c02343f695
vipin030 <vipin030@gmail.com>
2015-02-23 18:36:53 +0530

    First Commit

---
+++ data/content.js
@@ -0,0 +1,12 @@
+
+var j = document.createElement('script');
+j.src = 'jquery-1.10.2.min.js';
+(document.head || document.body || document.documentElement).appendChild(j);
+
+var g = document.createElement('script');
+g.src = 'gmail.js';
+(document.head || document.body || document.documentElement).appendChild(g);
+
+var s = document.createElement('script');
+s.src = 'mail_function.js';
+(document.head || document.body || document.documentElement).appendChild(s);
